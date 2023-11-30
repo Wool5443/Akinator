fa1a6090519cb06ca90a020fe706c48c90c344e5
Wool5443 <mihsolodilov2015@gmail.com>
2023-11-30 19:16:22 +0300

    Fixed compare

--- src/Akinator.cpp
+++ src/Akinator.cpp
@@ -57,7 +57,7 @@ do                                                                              
 {                                                                                   \
     char _toSay[2 * MAX_STRING_LENGTH + MAX_COMPARE_STRING_LENGTH] = "";            \
     sprintf(_toSay, __VA_ARGS__);                                                   \
-    printf("%s\n", _toSay);                                                         \
+    fputs(_toSay, stdout);                                                          \
     char _command[sizeof(_toSay) + COMMAND_STUFF_LENGTH] = "";                      \
     sprintf(_command, "echo '(SayText \"%s\")' "                                    \
     "| festival --pipe", _toSay);                                                   \
@@ -120,13 +120,13 @@ char _retryInput(const char* options);
 
 ErrorCode Play(Tree* dataTree)
 {
-    PRINT_SAY("Hello, bag of flash! I am the smartest intelligence in the Universe!");
-    PRINT_SAY("Due to my generousity I am willing to show you how smart I am.");
-    PRINT_SAY("Tell me what to show you:");
-    PRINT_SAY("Type g if you want me to guess whats on your mind.");
-    PRINT_SAY("Type c if you want me to compare two objects.");
-    PRINT_SAY("Type d if you want me to describe an object.");
-    PRINT_SAY("Type q if you want to stay dumb.");
+    PRINT_SAY("Hello, bag of flash! I am the smartest intelligence in the Universe!\n");
+    PRINT_SAY("Due to my generousity I am willing to show you how smart I am.\n");
+    PRINT_SAY("Tell me what to show you:\n");
+    PRINT_SAY("Type g if you want me to guess whats on your mind.\n");
+    PRINT_SAY("Type c if you want me to compare two objects.\n");
+    PRINT_SAY("Type d if you want me to describe an object.\n");
+    PRINT_SAY("Type q if you want to stay dumb.\n");
 
     char userAnswer = '\0';
     if (scanf("%c", &userAnswer) != 1)
@@ -156,7 +156,7 @@ ErrorCode Play(Tree* dataTree)
             userAnswer = _retryInput("g, q");
             break;
         }
-        PRINT_SAY("What do you want to do next?");
+        PRINT_SAY("What do you want to do next?\n");
         if (scanf("%c", &userAnswer) != 1)
             return EXIT;
         ClearBuffer(stdin);
@@ -175,10 +175,13 @@ ErrorCode _recGuess(TreeNode* node)
 
     if (node->left)
     {
-        PRINT_SAY("Is it %s?", node->value);
+        PRINT_SAY("Is it %s?\n", node->value);
         char userAnswer = '\0';
         if (scanf("%c", &userAnswer) != 1)
+        {
+            PRINT_SAY("You left me, traitor!\n");
             return EXIT;
+        }
         ClearBuffer(stdin);
         while (true)
         {
@@ -191,8 +194,8 @@ ErrorCode _recGuess(TreeNode* node)
         }
     }
 
-    PRINT_SAY("Let me guess...");
-    PRINT_SAY("Its %s, innit?", node->value);
+    PRINT_SAY("Let me guess... ");
+    PRINT_SAY("Its %s, innit?\n", node->value);
 
     char userAnswer = '\0';
     if (scanf("%c", &userAnswer) != 1)
@@ -203,7 +206,7 @@ ErrorCode _recGuess(TreeNode* node)
         SWITCH_Y_N(
         userAnswer,
 
-        PRINT_SAY("As I said!");
+        PRINT_SAY("As I said!\n");
         putchar('\n');
         return EVERYTHING_FINE,
 
@@ -219,14 +222,14 @@ ErrorCode _addNewObject(TreeNode* node)
 {
     MyAssertSoft(node, ERROR_NULLPTR);
 
-    PRINT_SAY("Tell me who it was.");
+    PRINT_SAY("Tell me who it was.\n");
     char* object = (char*)calloc(MAX_STRING_LENGTH + 1, 1);
     fgets(object, MAX_STRING_LENGTH, stdin);
 
     object[strlen(object) - 1] = '\0';
 
-    PRINT_SAY("How are %s and %s different?", node->value, object);
-    PRINT_SAY("%s is", object);
+    PRINT_SAY("How are %s and %s different?\n", node->value, object);
+    PRINT_SAY("%s is ", object);
 
     char* difference = (char*)calloc(MAX_STRING_LENGTH + 1, 1);
     fgets(difference, MAX_STRING_LENGTH, stdin);
@@ -251,7 +254,7 @@ ErrorCode _compare(Tree* dataTree)
     MyAssertSoft(dataTree, ERROR_NULLPTR);
     ERR_DUMP_RET(dataTree);
 
-    PRINT_SAY("What objects do you want to compare?");
+    PRINT_SAY("What objects do you want to compare?\n");
     char obj1[MAX_STRING_LENGTH] = "";
     fgets(obj1, MAX_STRING_LENGTH, stdin);
     char obj2[MAX_STRING_LENGTH] = "";
@@ -265,14 +268,18 @@ ErrorCode _compare(Tree* dataTree)
     RETURN_ERROR(stackRes1.error);
     Stack* stack1 = stackRes1.value;
     
+    dataTree->Dump();
     TreeNodeResult nodeRes1 = _recFindNode(dataTree->root, stack1, obj1);
+    dataTree->Dump();
     RETURN_ERROR(nodeRes1.error);
 
     StackResult stackRes2 = StackInit();
     RETURN_ERROR(stackRes2.error);
     Stack* stack2 = stackRes2.value;
-    
+
+    dataTree->Dump();
     TreeNodeResult nodeRes2 = _recFindNode(dataTree->root, stack2, obj2);
+    dataTree->Dump();
     RETURN_ERROR(nodeRes2.error);
 
     StackElementResult obj1Pop = Pop(stack1);
@@ -284,17 +291,17 @@ ErrorCode _compare(Tree* dataTree)
     TreeNode* curNode = dataTree->root;
     if (obj1Pop.value == obj2Pop.value)
     {
-        PRINT_SAY("%s and %s are similar because they", obj1, obj2);
+        PRINT_SAY("%s and %s are similar because they\n", obj1, obj2);
 
         while (!obj1Pop.error && !obj2Pop.error && obj1Pop.value == obj2Pop.value)
         {
             FOLLOW_PATH(obj1Pop.value, 
             {
-                PRINT_SAY("are %s", curNode->value);
+                PRINT_SAY("are %s\n", curNode->value);
                 curNode = curNode->left;
             },
             {
-                PRINT_SAY("are not %s", curNode->value);
+                PRINT_SAY("are not %s\n", curNode->value);
                 curNode = curNode->right;
             });
 
@@ -303,13 +310,13 @@ ErrorCode _compare(Tree* dataTree)
         }
     }
 
-    PRINT_SAY("But...");
+    PRINT_SAY("But...\n");
 
     if (!obj1Pop.error)
     {
         TreeNode* curNode1 = curNode;
 
-        PRINT_SAY("Unlike %s", obj2);
+        PRINT_SAY("Unlike %s\n", obj2);
 
         RETURN_ERROR(_describeObject(curNode1, stack1, obj1, obj1Pop));
     }
@@ -318,7 +325,7 @@ ErrorCode _compare(Tree* dataTree)
     {
         TreeNode* curNode2 = curNode;
 
-        PRINT_SAY("Unlike %s", obj1);
+        PRINT_SAY("Unlike %s\n", obj1);
 
         RETURN_ERROR(_describeObject(curNode2, stack2, obj2, obj2Pop));
     }
@@ -334,7 +341,7 @@ ErrorCode _describe(Tree* dataTree)
     MyAssertSoft(dataTree, ERROR_NULLPTR);
     ERR_DUMP_RET(dataTree);
 
-    PRINT_SAY("What object do you want me to describe?");
+    PRINT_SAY("What object do you want me to describe?\n");
     char obj[MAX_STRING_LENGTH] = "";
     fgets(obj, MAX_STRING_LENGTH, stdin);
 
@@ -363,17 +370,17 @@ ErrorCode _describeObject(TreeNode* node, Stack* stack, const char* obj,
 
     if (!objPop.error)
     {
-        PRINT_SAY("%s", obj);
+        PRINT_SAY("%s\n", obj);
 
         while (!objPop.error)
         {
             FOLLOW_PATH(objPop.value,
             {
-                PRINT_SAY("is %s", curNode->value); 
+                PRINT_SAY("is %s\n", curNode->value); 
                 curNode = curNode->left;
             },
             {
-                PRINT_SAY("is not %s", curNode->value);
+                PRINT_SAY("is not %s\n", curNode->value);
                 curNode = curNode->right;
             });
             objPop = Pop(stack);
@@ -460,6 +467,8 @@ TreeNodeResult _recFindNode(TreeNode* node, Stack* stack, const char* obj)
         }
     }
 
+    node->id = oldId;
+    
     return { nullptr, ERROR_NOT_FOUND };
 }
 
@@ -482,7 +491,7 @@ ErrorCode _pushPath(Stack* stack, TreeNode* node)
 
 char _retryInput(const char* options)
 {
-    PRINT_SAY("Incorrect option. Availible options are %s. Try again.", options);
+    PRINT_SAY("Incorrect option. Availible options are %s. Try again.\n", options);
     char userAnswer = '\0';
     if (scanf("%c", &userAnswer) != 1)
         return EXIT;
--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 
 int main()
 {
+    Tree::StartHtmlLogging();
+
     Tree akinator = {};
 
     akinator.Read(AKINATOR_DATA);
-
-    akinator.Dump();
 
     ErrorCode playError = Play(&akinator);
     if (playError != EVERYTHING_FINE && playError != EXIT)
         return playError;
 
-    akinator.Dump();
-
-    // akinator.Print(AKINATOR_DATA);
+    akinator.Print("garbage.txt");
 
     akinator.Destructor();
+
+    Tree::EndHtmlLogging();
 
     return 0;
 }
